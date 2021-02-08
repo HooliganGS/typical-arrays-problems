be790ddb85f30a2aa8a4f6f040d79dff83ed6a23
HooliganGS <gaga.borovikov.ilia12@gmail.com>
2021-02-08 10:38:36 +0300

    feat: complite max min avg without empty

--- src/index.js
+++ src/index.js
@@ -1,11 +1,47 @@
 
 exports.min = function min (array) {
-  return Math.min(array);
+  if (array.length === 0)
+  {
+    return 0;
+  }
+ let min = array[0];
+ for (let i=0; i < array.length; i++)
+ {
+   if (array[i] < min)
+   {
+     min = array[i];
+   }
+ }
+ return min;
 }
 
 exports.max = function max (array) {
-  return Math.max(array);
+  if (array.length === 0)
+  {
+    return 0;
+  }
+  let max = array[0];
+  for (let i=0; i < array.length; i++)
+  {
+    if (array[i] > max)
+    {
+      max = array[i];
+    }
+  }
+  return max;
 }
+
 exports.avg = function avg (array) {
-  return 0;
+  if (array.length === 0)
+  {
+    return 0;
+  }
+  let sum = 0;
+  let ag =0;
+  for (let i=0; i < array.length; i++)
+  {
+    sum  = sum + array[i];
+  }
+  ag = sum / array.length;
+  return ag;
 }
